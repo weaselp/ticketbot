8757547bc685f42aed4d86c27a7d9f63fd181890
Peter Palfrader <peter@palfrader.org>
2016-04-11 11:22:02 +0200

    drop ugly semicolon

--- ticketconfig.py
+++ ticketconfig.py
@@ -108,10 +108,10 @@ class TicketConfig:
         self.providers['labs.riseup.net'].addChannel('#tails*', default=True)
 
         self.providers['bugs.debian.org'].addChannel('#tor-test', regex='(?<!\w)[dD]#([0-9]{4,})(?:(?=\W)|$)')
-        self.providers['bugs.debian.org'].addChannel('#debian-*', default=True);
+        self.providers['bugs.debian.org'].addChannel('#debian-*', default=True)
         self.providers['rt.debian.org'  ].addChannel('#debian-*', regex='(?<!\w)RT#([0-9]+)(?:(?=\W)|$)')
 
-        self.providers['bugs.debian.org'].addChannel('#pbuilder', default=True);
+        self.providers['bugs.debian.org'].addChannel('#pbuilder', default=True)
         self.providers['launchpad.net/ubuntu'].addChannel('#pbuilder', regex='(?<!\w)[uU]#([0-9]{4,})(?:(?=\W)|$)')
 
     def __init__(self):
