6fed6daebe3f11d22fb4ec3803ceb7fba8ede60e
Peter Palfrader <peter@palfrader.org>
2019-07-22 14:31:28 +0200

    Make all #deb* channels Debian

--- ticketconfig.py
+++ ticketconfig.py
@@ -118,7 +118,7 @@ class TicketConfig:
 
         self.providers['labs.riseup.net'].addChannel('#tails*', default=True)
 
-        for ch in ('#debian-*', '#pbuilder', '#devscripts', '#reproducible-builds', '#debci'):
+        for ch in ('#debian-*', '#pbuilder', '#devscripts', '#reproducible-builds', '#deb*'):
             self.providers['bugs.debian.org'].addChannel(ch, default=True)
             self.providers['rt.debian.org'  ].addChannel(ch, regex='(?<!\w)RT#([0-9]+)(?:(?=\W)|$)')
 
